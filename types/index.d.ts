dee5015e44695e30b9e966ec3c7c5f975a085adc
TotallyLumi <totallylumi@proton.me>
2026-07-03 22:56:17 +0100

    [code]: Added all interfaces

--- types/index.d.ts
+++ types/index.d.ts
@@ -14,4 +14,33 @@ import {
 import type { ILogObj } from "tslog";
 import type { CustomLogger } from "../utils/logger";
 
-export interface BotClient extends Client {}
+export interface BotClient extends Client {
+  settings: {
+    prefix: string;
+    color: ColorResolvable;
+  };
+  logger: CustomLogger<ILogObj>;
+  commands: Collection<string, MessageCommand>;
+}
+
+export interface SlashCommand {}
+
+export interface MessageCommand {}
+
+export interface BotEvent {}
+
+export interface Modal {}
+
+export interface App {}
+
+export interface Handler {}
+
+export interface Config {}
+
+declare global {
+  interface String {
+    brightYellow: string;
+    brightGreen: string;
+    brightBlue: string;
+  }
+}
