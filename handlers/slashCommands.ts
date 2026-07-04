f5083bb9d38be05ed1b8c4f5f0327c0277732fc8
TotallyLumi <totallylumi@proton.me>
2026-07-04 14:38:19 +0100

    [code]: Finished Slash Command Suport and added kick

--- handlers/slashCommands.ts
+++ handlers/slashCommands.ts
@@ -7,6 +7,9 @@ const args = process.argv.slice(2);
 
 const api = new REST({ version: "10" }).setToken(client.settings.token);
 
+const CLIENT_ID = "1522705277458055399";
+const TEST_GUILD = "1522707753267892275";
+
 async function getSlashCommands(client: BotClient | null) {
   const folder = fs.readdirSync("./commands/slash");
   const slash_commands: any[] = [];
@@ -48,3 +51,45 @@ async function getSlashCommands(client: BotClient | null) {
 
   return slash_commands;
 }
+
+async function registerCommands(
+  client: BotClient | null,
+  dev: boolean = false,
+) {
+  const slashCommands = await getSlashCommands(client);
+
+  const body = [...slashCommands];
+
+  try {
+    if (dev) {
+      await api.put(Routes.applicationGuildCommands(CLIENT_ID, TEST_GUILD), {
+        body,
+      });
+    } else {
+      await api.put(Routes.applicationCommands(CLIENT_ID), { body });
+    }
+
+    client?.logger.success("Successfully Registered Slash Commands");
+  } catch (error) {
+    client?.logger.error(error);
+  }
+}
+
+if (args[0] == "sync") {
+  (async () => {
+    await registerCommands(null, false);
+  })();
+} else {
+  async () => {
+    await registerCommands(null, TEST_GUILD !== null);
+  };
+}
+
+const handler: Handler = {
+  name: "slashCommands",
+  execute: async (client: BotClient) => {
+    await registerCommands(client, TEST_GUILD !== null);
+  },
+};
+
+export default handler;
