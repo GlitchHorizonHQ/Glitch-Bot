cd2c1fb20e4423ecd022b9fb6ef30c564356bbd7
TotallyLumi <totallylumi@proton.me>
2026-07-04 12:36:44 +0100

    [code]: Message Command handler and Ping Command

---
+++ commands/message/info/ping.ts
@@ -0,0 +1,17 @@
+import { EmbedBuilder, Message } from "discord.js";
+import type { BotClient, MessageCommand } from "../../../types";
+
+const command: MessageCommand = {
+  name: "ping",
+  description:
+    "Displays the current ping of the bot's websocket connection to the API.",
+  execute: async (client: BotClient, message: Message, args: string[]) => {
+    const embed = new EmbedBuilder()
+      .setColor(client.settings.color)
+      .setDescription(`🏓 Ping: ${Math.round(client.ws.ping)} ms`);
+
+    message.reply({ embeds: [embed] });
+  },
+};
+
+export default command;
---
+++ handlers/messageCommands.ts
@@ -0,0 +1,36 @@
+import fs from "fs";
+import type { BotClient, MessageCommand, Handler } from "../types";
+
+const handler: Handler = {
+  name: "messageCommands",
+  execute: async (client: BotClient) => {
+    const folder = fs.readdirSync("./commands/message");
+
+    for (const dir of folder) {
+      const commands = fs
+        .readdirSync(`./commands/message/${dir}`)
+        .filter(
+          (f) => (f.endsWith(".js") || f.endsWith(".ts")) && !f.startsWith("-"),
+        );
+
+      for (const file of commands) {
+        const fileWithoutExtensions = file.replace(/\.[.jt]s$/, "");
+        const commandModule = await import(
+          `../commands/message/${dir}/${fileWithoutExtensions}.js`
+        );
+        const command: MessageCommand = commandModule.default || commandModule;
+
+        if (command) {
+          command["category"] = dir;
+          client.commands.set(command.name, command);
+
+          if (command.aliases && Array.isArray(command.aliases)) {
+            command.aliases.forEach((a) => client.aliases.set(a, command.name));
+          }
+        }
+      }
+    }
+  },
+};
+
+export default handler;
